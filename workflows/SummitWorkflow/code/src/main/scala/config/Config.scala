85dd8164fbf02c8fabc45cefda8b0e13f1ee5b1a
Prophecy Demo <demo5@simpledatalabs.com>
2021-05-06 05:52:55 +0000

    Deleting content

--- workflows/SummitWorkflow/code/src/main/scala/config/Config.scala
+++
@@ -1,9 +0,0 @@
-package config
-
-import io.prophecy.libs._
-import pureconfig.ConfigReader.Result
-import pureconfig._
-import pureconfig.generic.ProductHint
-import pureconfig.generic.auto._
-
-case class Config(fabricName: String) extends ConfigBase
